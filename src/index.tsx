337ce2ecb1b4efa008a47fbba7237487d8d6ccb2
Wahyu Agus Arifin - IP <68760662+ItPohgero@users.noreply.github.com>
2024-04-22 15:34:12 +0700

    up

--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,7 @@
-// src/index.tsx
 import "./global.css";
 
-const Demo = () => {
-    return <div className="p-4 text-white bg-red-800">demos</div>;
+const LavaUI = () => {
+    return <div className="p-4 text-white bg-red-800 w-max rounded-full">Lava UI</div>;
 };
 
-export default Demo;
+export default LavaUI;
--- src/libs/Button.tsx
+++ src/libs/Button.tsx
@@ -1,10 +1,8 @@
 import React from 'react';
 import { Button } from 'antd';
 
-const LavaButton: React.FC = () => (
+export const LavaButton: React.FC = () => (
     <div className="Button">
         <Button type="primary">Button</Button>
     </div>
 );
-
-export default LavaButton;
