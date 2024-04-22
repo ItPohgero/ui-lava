685b2b601a0fd44acd3f53330cf90f08c6fc3e78
Wahyu Agus Arifin - IP <68760662+ItPohgero@users.noreply.github.com>
2024-04-22 15:29:41 +0700

    up

---
+++ src/libs/Button.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import { Button } from 'antd';
+
+const LavaButton: React.FC = () => (
+    <div className="Button">
+        <Button type="primary">Button</Button>
+    </div>
+);
+
+export default LavaButton;
